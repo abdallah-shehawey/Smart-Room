efbc870f6beda52397997e3b0b2e46dcecf62d84
abdallah-shehawey <shehawey9@gmail.com>
2024-10-24 00:10:38 +0300

    Smart Room - AVR Project

---
+++ Smart_Room/Release/MCAL_Layer/DIO/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../MCAL_Layer/DIO/DIO_prog.c 
+
+OBJS += \
+./MCAL_Layer/DIO/DIO_prog.o 
+
+C_DEPS += \
+./MCAL_Layer/DIO/DIO_prog.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+MCAL_Layer/DIO/%.o: ../MCAL_Layer/DIO/%.c MCAL_Layer/DIO/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
